da899b1f1ffc326bca5f5bd0ff943acc93da1111
Dezhao liu(EXTERN:CleNET) <extern.dezhao.liu@cariad-technology.cn>
2024-12-22 20:14:22 +0800

    init

---
+++ vars/printdemo.groovy
@@ -0,0 +1,8 @@
+// 在 Jenkins 共享库的文件中定义一个名为 'call' 的方法
+def call(Map config) {
+    // 打印传入的配置
+    echo "Hello, ${config.name}! Welcome to Jenkins Pipeline."
+    echo "Job URL: ${config.url}"
+    echo "Build Number: ${config.buildNumber}"
+}
+
